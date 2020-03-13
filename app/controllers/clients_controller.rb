6fb0eac16944b5f0d7f24973bd22617dfa7a78f9
BMWeygant <bobbydigital108@gmail.com>
2020-03-13 15:22:14 -0400

    Added strong_params and base controller methods.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,26 @@
 class ClientsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:name, :vip, :city, :state, :twitter, :facebook, :instagram, :snapchat)
+    end
+
 end
