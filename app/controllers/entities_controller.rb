452792474009bc9a014edb3acfb7720c8a0db42d
BMWeygant <bobbydigital108@gmail.com>
2020-03-13 15:24:34 -0400

    Added base methods & strong params

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,25 @@
 class EntitiesController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def entity_params
+        params.require(:entity).permit(:name, :industry, :description, :services, :website, :phone, :email, :city, :state, :twitter, :facebook, :instagram, :snapchat)
+    end
 end
